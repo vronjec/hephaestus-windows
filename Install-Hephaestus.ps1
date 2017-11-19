2e2c84191843d7feb9123955f65195fdb2eb50f7
Viktor Ronjec <viktor@ronjec.io>
2017-11-19 21:45:37 +0100

    Add file extension to executable paths

--- Install-Hephaestus.ps1
+++ Install-Hephaestus.ps1
@@ -23,19 +23,19 @@ Workflow Install-Hephaestus
         Sequence {
             # Install latest Chrome
             Invoke-WebRequest "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile "$env:temp\ChromeSetup.exe"
-            Start-Process -FilePath "$env:temp\ChromeSetup" -ArgumentList "/install /silent" -Wait
+            Start-Process -FilePath "$env:temp\ChromeSetup.exe" -ArgumentList "/install /silent" -Wait
 
             # Install latest Firefox
             Invoke-WebRequest "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US" -OutFile "$env:temp\FirefoxSetup.exe"
-            Start-Process -FilePath "$env:temp\FirefoxSetup" -ArgumentList "-ms" -Wait
+            Start-Process -FilePath "$env:temp\FirefoxSetup.exe" -ArgumentList "-ms" -Wait
 
             # Install latest Opera
             Invoke-WebRequest "https://net.geo.opera.com/opera/stable/windows" -OutFile "$env:temp\OperaSetup.exe"
-            Start-Process -FilePath "$env:temp\OperaSetup" -ArgumentList "/silent /launchopera 0" -Wait
+            Start-Process -FilePath "$env:temp\OperaSetup.exe" -ArgumentList "/silent /launchopera 0" -Wait
 
             # Install latest LastPass browser extensions
             Invoke-WebRequest "https://lastpass.com/download/cdn/lastpass_x64.exe" -OutFile "$env:temp\LastPassSetup.exe"
-            Start-Process -FilePath "$env:temp\LastPassSetup" -ArgumentList "--silinstall --userinstallff --userinstallie --noaddremove --nostartmenu --nohistory" -Wait
+            Start-Process -FilePath "$env:temp\LastPassSetup.exe" -ArgumentList "--silinstall --userinstallff --userinstallie --noaddremove --nostartmenu --nohistory" -Wait
         } # Sequence
 
     } # Parallel
