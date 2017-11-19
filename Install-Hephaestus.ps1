49b5a7026c8e55d932962f15836c5c3f632593d7
Viktor Ronjec <viktor@ronjec.io>
2017-11-19 21:20:05 +0100

    Add web browser-related instructions to installation script

--- Install-Hephaestus.ps1
+++ Install-Hephaestus.ps1
@@ -20,6 +20,24 @@ Workflow Install-Hephaestus
             Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -Online -All -LimitAccess -NoRestart -ErrorAction Stop
         } # Sequence
 
+        Sequence {
+            # Install latest Chrome
+            Invoke-WebRequest "https://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile "$env:temp\ChromeSetup.exe"
+            Start-Process -FilePath "$env:temp\ChromeSetup" -ArgumentList "/install /silent" -Wait
+
+            # Install latest Firefox
+            Invoke-WebRequest "https://download.mozilla.org/?product=firefox-latest-ssl&os=win64&lang=en-US" -OutFile "$env:temp\FirefoxSetup.exe"
+            Start-Process -FilePath "$env:temp\FirefoxSetup" -ArgumentList "-ms" -Wait
+
+            # Install latest Opera
+            Invoke-WebRequest "https://net.geo.opera.com/opera/stable/windows" -OutFile "$env:temp\OperaSetup.exe"
+            Start-Process -FilePath "$env:temp\OperaSetup" -ArgumentList "/silent /launchopera 0" -Wait
+
+            # Install latest LastPass browser extensions
+            Invoke-WebRequest "https://lastpass.com/download/cdn/lastpass_x64.exe" -OutFile "$env:temp\LastPassSetup.exe"
+            Start-Process -FilePath "$env:temp\LastPassSetup" -ArgumentList "--silinstall --userinstallff --userinstallie --noaddremove --nostartmenu --nohistory" -Wait
+        } # Sequence
+
     } # Parallel
 
     # Enable System Restore
